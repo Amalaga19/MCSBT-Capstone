eed3bcc4467e32522bb215963e1fae8ee56ea3f2
Amalaga19 <andres.malaga1@gmail.com>
2024-03-25 15:56:54 +0100

    Fixed cors issue in backend and deployed.

--- backend/main.py
+++ backend/main.py
@@ -55,7 +55,7 @@ def add_cors_headers(response):
     if origin:
         response.headers['Access-Control-Allow-Origin'] = origin
         response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     return response
 
